e94d800eb1b8a37f6e32309de12c313296feee0d
John <j.yakubison@gmail.com>
2021-04-20 16:04:07 -0400

    Added other actions to saga ref, and began data entry for fullRound

---
+++ js/fullRound_data.js
@@ -0,0 +1,14 @@
+fullRound_data = [
+    {
+        title: "Coup De Grace",
+        icon: "heart-stake",
+        subtitle: "Finish a helpless creature",
+        description: "Melee Weapon attack on Helpless Creature to Critical Hit",
+        lines: [
+            "As a Full-Round Action, you can use a Melee Weapon to deliver a Coup de Grace to a Helpless creature or Droid.",
+            "You can also use a Ranged Weapon, provided you are adjacent to the target. You automatically score a Critical Hit,",
+            "dealing double Damage. A defender reduced to 0 Hit Points by a Coup de Grace dies instantly, or, in the case of a Droid,",
+            "is destroyed. An Unconscious or Disabled defender Hit by a Coup de Grace also dies or is destroyed instantly."
+        ]
+    }
+]
---
+++ js/sagaref.js
@@ -0,0 +1,8 @@
+function init(){
+    
+}
+
+// {name of data in separate js file, parentId
+function fillSection(data, parentId, type){
+
+}
