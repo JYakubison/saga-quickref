59b199dcef74713ff370f0aa2cee74f08a4e9e40
John <j.yakubison@gmail.com>
2021-04-20 16:19:50 -0400

    Added info for full round actions

--- js/fullRound_data.js
+++ js/fullRound_data.js
@@ -6,9 +6,43 @@ fullRound_data = [
         description: "Melee Weapon attack on Helpless Creature to Critical Hit",
         lines: [
             "As a Full-Round Action, you can use a Melee Weapon to deliver a Coup de Grace to a Helpless creature or Droid.",
-            "You can also use a Ranged Weapon, provided you are adjacent to the target. You automatically score a Critical Hit,",
-            "dealing double Damage. A defender reduced to 0 Hit Points by a Coup de Grace dies instantly, or, in the case of a Droid,",
-            "is destroyed. An Unconscious or Disabled defender Hit by a Coup de Grace also dies or is destroyed instantly."
+            "You can also use a Ranged Weapon, provided you are adjacent to the target. ",
+            "You automatically score a Critical Hit, dealing double Damage. ",
+            "A defender reduced to 0 Hit Points by a Coup de Grace dies instantly, or, in the case of a Droid, is destroyed. ",
+            "An Unconscious or Disabled defender Hit by a Coup de Grace also dies or is destroyed instantly."
+        ]
+    },
+    {
+        title: "Run",
+        icon: "sprint",
+        subtitle: "Move 4x your speed",
+        description: "Move up to 4x your speed depending on armor and encumbrance",
+        lines: [
+            "You can Run as a Full-Round Action, moving up to four times your Speed in a straight line (Or three times your Speed in a straight line if wearing Heavy Armor or carrying a Heavy Load).",
+            "See the Endurance skill for Running rules. "
+        ]
+    },
+    {
+        title: "2 Weapon Attack",
+        icon: "light-sabers",
+        subtitle: "Attacking with two weapons",
+        description: "Attacking with two weapons for a penalty",
+        lines: [
+            "As a Full-Round Action, a character armed with two weapons can attack once with each weapon, but the character takes a -10 penalty on all attacks for the Round.",
+            "This penalty assumes that the character is proficient with the weapon in hand; apply an additional -5 penalty on the attack roll if the character is not proficient with the weapon.",
+            "A character armed with three or more weapons still only gains one extra attack, but that character may choose which weapon it wishes to use for this extra attack each Round. "
+        ]
+    },
+    {
+        title: "Double Weapon Attack",
+        icon: "scalpel-strike",
+        subtitle: "Attacking with a double weapon",
+        description: "Attacking with a weapon that has multiple attacks",
+        lines: [
+            "As a Full-Round Action, a character armed with a double weapon (Such as a Double-Bladed Lightsaber) can attack once with each end of the weapon, but the character takes a -10 penalty penalty on all attacks for the Round. ",
+            "This penalty assumes that the character is proficient with the weapon in hand; apply an additional -5 penalty on the attack roll if the character is not proficient with the weapon.",
+            "(A character who chooses to attack with only one end of a double weapon can do so as a Standard Action.) "
         ]
     }
+
 ]
